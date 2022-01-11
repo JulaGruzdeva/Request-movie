7d307d4c62e824ab85b90b9858c8683b23b6464b
Julia Gruzdeva <yulasha.gruzdeva@gmail.com>
2022-01-11 15:01:00 +0300

    add persons inputs in loop

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?: ');
-const latestFilms = prompt('Один из последних просмотренных фильмов?'),
-      scoreFilms = prompt('На сколько оцените его?');
+
+
 
 
 let personalMovieDB = {
@@ -13,6 +13,27 @@ let personalMovieDB = {
     privat: false,
 }
 
-personalMovieDB.movies[latestFilms] = scoreFilms;
+for (let i = 0; i<2; i++){
+    const latestFilms = prompt('Один из последних просмотренных фильмов?'),
+      scoreFilms = prompt('На сколько оцените его?');
+
+    if (latestFilms != null && scoreFilms != null && latestFilms != '' && scoreFilms != '' && latestFilms.length < 50){
+        personalMovieDB.movies[latestFilms] = scoreFilms;
+        console.log('done');
+    } else {
+        console.log('error');
+        --i
+    }
+    
+}
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотренно довольно мало фильмов');
+}else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+}else if(personalMovieDB.count > 30){
+    console.log('Вы киноман');
+}else console.log('error');
 
 console.log(personalMovieDB);
+2
